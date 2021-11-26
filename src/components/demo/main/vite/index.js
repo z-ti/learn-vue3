f75c7ac2c6882dc4335d705aa57de28a440f9917
huzhifu <huzhifu@hnnewsoft.com>
2021-11-26 14:11:27 +0800

    feat:cicd部署:dog:dog:

--- src/components/demo/main/vite/index.js
+++ src/components/demo/main/vite/index.js
@@ -58,4 +58,33 @@
  *  创建mock文件，并写入mock数据
  *  修改package.json "dev": "cross-env NODE_ENV=development vite",
  *   
+ * 打包部署
+ *  使用 npm run build 执行打包 生成dist目录
+ *  传统方式 通过scp，ssh的方式将dist 上传到服务器，再配置好nginx
+ *  利用github actions实现CI/CD过程
+ *    流程：
+ *      开发机 ->  push -> github(checkout & build) -> 虚拟机 -> upload -> 阿里云服务器
+ *    准备：
+ *      阿里云linux服务器（linux操作/阿里云相关操作等）
+ *    第一步：
+ *      配置workflow，在项目根目录下创建 .github/workflows/publish.yml
+ *    第二步：
+ *      在github当前项目下设置私钥选项 Secrets -> New repository secrets
+ *      复制本地私钥， ～/.ssh/id_rsa 放入github输入信息里
+ *    第三步：
+ *      在阿里云服务器配置nginx
+ *      登陆服务器 ssh root@47.98.252.43 # IP 换成自己的
+ *      配置nginx cd /etc/nginx/sites-enabled/   vi vue-in-action
+ *      添加如下配置 
+ *         server {
+ *            listen 8080;
+ *            server_name 47.98.252.43;
+ *            location / {
+ *              root /root/vue-in-action/dist/;
+ *              index index.html index.htm;
+ *            }
+ *         }
+ *      重启nginx   nginx -s reload
+ *    第四步：
+ *      push 代码，触发workflow
  */
